029e405c22b1f2d587f713dad24defd551fc8166
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2020-11-23 21:46:13 -0700

    Add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { render, screen } from "@testing-library/react";
 
-it("exists", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe("App component", () => {
+  it("exists", () => {
+    render(<App />);
+  });
+
+  it("renders default greeting", () => {
+    render(<App />);
+    expect(screen.getByText("Hello World!")).toBeInTheDocument();
+  });
+
+  it("renders another greeting", () => {
+    const greeting = "Hi friend!";
+    render(<App greeting={greeting} />);
+    expect(screen.getByText(greeting)).toBeInTheDocument();
+  });
 });
